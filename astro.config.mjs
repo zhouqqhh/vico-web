23d45bf9af2a9916c906c48d8b880c6329570d4b
Qinhong Zhou <zhouqqhh@gmail.com>
2025-05-29 10:58:27 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://weebao.github.io',
+  site: 'https://zhouqqhh.github.io',
   base: '/vico-web',
   vite: {
     plugins: [tailwindcss()]
